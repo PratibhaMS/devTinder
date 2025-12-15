a23dcced8cf81b767ca5f4180c55303c2d28aa04
Suresh Kukumati <kml.surya29@gmail.com>
2025-12-15 17:57:05 +0530

    user schema update with new field and validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,44 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  emailId: { type: String },
-  password: { type: String },
-  age: { type: Number },
-  gender: { type: String },
-});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true, minLength: 5, maxLength: 50 },
+    lastName: { type: String, minLength: 5, maxLength: 50 },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true },
+    age: { type: Number, min: 18 },
+    gender: {
+      type: String,
+      validate(v) {
+        if (!["male", "female", "others"].includes(v)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+      minLength: 4,
+      maxLength: 6,
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiqHWswvbMIMtxVPUmmLU_F0sNrf-l4J5z4w&s",
+    },
+    about: {
+      type: String,
+      default: "This is default about us for user",
+      minLength: 20,
+      maxLength: 200,
+    },
+    skills: { type: [String] },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // const User = mongoose.model("User", userSchema);
 module.exports = mongoose.model("User", userSchema);
