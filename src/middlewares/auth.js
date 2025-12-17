810bd13653e19177bdcaaabc5901e44ccd1b1811
Suresh Kukumati <kml.surya29@gmail.com>
2025-12-17 20:41:55 +0530

    new package added jsonwebtoken and cookies parser and userAuth middleware created

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,11 +1,25 @@
-const adminAuth = (req, res, next) => {
-  const token = "xyz";
-  const isAdminAuthorized = token == "xyz";
-  if (!isAdminAuthorized) {
-    res.status(401).send("Unauthorised request");
-  } else {
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const userAuth = async (req, res, next) => {
+  try {
+    // read the token from request cookies
+    let { token } = req.cookies;
+    if (!token) {
+      throw new Error("Invalid token");
+    }
+    // validate token
+    let decodedMess = await jwt.verify(token, "DEV@Tinder");
+    const { _id } = decodedMess;
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    req.user = user;
     next();
+  } catch (error) {
+    res.status(400).send("Error " + error.message);
   }
 };
 
-module.exports = { adminAuth };
+module.exports = { userAuth };
